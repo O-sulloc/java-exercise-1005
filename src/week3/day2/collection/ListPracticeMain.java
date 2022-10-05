49a1fcf0f07473c40d5ba7fd828a7a065402f34c
O-sulloc <bujjaf@gmail.com>
2022-10-05 16:19:01 +0900

    list study commit

--- src/week3/day2/collection/ListPracticeMain.java
+++ src/week3/day2/collection/ListPracticeMain.java
@@ -6,9 +6,15 @@ public class ListPracticeMain {
     public static void main(String[] args) {
         LikeLion2th likeLion2th = new LikeLion2th();
         List<String> students = likeLion2th.getStudentList();
+        List<Student> studentObjs = likeLion2th.getStudentObjs();
 
+        //for each문이랑 깥은거.. iter쓰면 바로 자동완성뜸 대박이다.
         for(String student:students){
             System.out.println(student);
         }
+
+        // list에 몇 개 들억아ㅣㅆ는지 세기.
+        System.out.println(students.size());
+
     }
 }
---
+++ src/week3/day2/collection/Student.java
@@ -0,0 +1,13 @@
+package week3.day2.collection;
+
+public class Student {
+    private int classNo;
+    private String name;
+    private String gitRepositoryAddress;
+
+    public Student(int classNo, String name, String gitRepositoryAddress) {
+        this.classNo = classNo;
+        this.name = name;
+        this.gitRepositoryAddress = gitRepositoryAddress;
+    }
+}
