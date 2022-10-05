09fe77ce8695129f9c1b2a9bfc265c112cb2d05e
O-sulloc <bujjaf@gmail.com>
2022-10-05 10:18:49 +0900

    cal commit

---
+++ src/CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        cal.plus(1,5);
+        cal.minus(5,2);
+        cal.multiple(12,5);
+        cal.divide(128,8);
+    }
+}
